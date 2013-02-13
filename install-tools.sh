b6144b4405ba47dd91a1a38b0f4b8b3ef2d75f40
Tal Salmona <tal.salmona@kenshoo.com>
2013-02-13 06:39:30 +0200

    Added a script to install some tools

---
+++ install-tools.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Install tools
+sudo apt-get -y install keepass2 meld dropbox everpad gimp curl wget espeak vlc skype
+
+#Install dev tools
+sudo apt-get -y install npm ruby1.9.1-full nodejs openjdk-7-jdk git git-cola git-svn git-flow subversion
+
